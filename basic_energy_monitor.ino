8ce69e94e8aea5275a30600fcf2f612a789887f8
Trick <trick@vanstaveren.us>
2020-12-18 15:19:49 -0600

    basic ethernet shield initialization

--- basic_energy_monitor.ino
+++ basic_energy_monitor.ino
@@ -3,23 +3,36 @@
 #include <Ethernet.h>
 #include <ArduinoOTA.h>
 
-
-
-// Include Emon Library
 EnergyMonitor emon1;
-// Create an instance
+
+byte mac[] = {
+  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02
+};
+
 void setup()
 {
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-  emon1.current(1, 111.1);             // Current: input pin, calibration.
+    if (Ethernet.begin(mac) == 0) {
+        Serial.println("Failed to configure Ethernet using DHCP");
+        if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+            Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+        } else if (Ethernet.linkStatus() == LinkOFF) {
+            Serial.println("Ethernet cable is not connected.");
+        }
+    }
+
+    Serial.print("My IP address: ");
+    Serial.println(Ethernet.localIP());
+
+    emon1.current(1, 111.1);             // Current: input pin, calibration.
 }
 
 void loop()
 {
-double Irms = emon1.calcIrms(1480);  // Calculate Irms only
-Serial.print(Irms*230.0);           // Apparent power
-  Serial.print(" ");
-  Serial.println(Irms);             // Irms
+    double Irms = emon1.calcIrms(1480);  // Calculate Irms only
+    Serial.print(Irms*230.0);           // Apparent power
+    Serial.print(" ");
+    Serial.println(Irms);             // Irms
 }
 
