ab7e5c5c78b6eaf8997cee8da497aff809c2b23e
Trick <trick@vanstaveren.us>
2020-12-18 22:48:52 -0600

    monitor just pins 0 and 1 for now

--- prometheus-arduino-current-sensor.ino
+++ prometheus-arduino-current-sensor.ino
@@ -62,53 +62,19 @@ String metrics() {
     message += "Content-type: text/plain\n\n";
     message += "# HELP sensor_watts Estimated watts from the sensor\n";
     message += "# TYPE sensor_watts gauge\n";
-    message += "sensor_watts{sensor=0} ";
+    message += "sensor_watts{sensor=\"0\"} ";
     message += last_watts0;
     message += "\n";
-    message += "# HELP sensor_amps Estimated amps from the sensor\n";
-    message += "# TYPE sensor_amps gauge\n";
-    message += "sensor_amps{sensor=0} ";
-    message += last_amps0;
-    message += "\n";
-    message += "# HELP sensor_watts Estimated watts from the sensor\n";
-    message += "# TYPE sensor_watts gauge\n";
-    message += "sensor_watts{sensor=1} ";
+    message += "sensor_watts{sensor=\"1\"} ";
     message += last_watts1;
     message += "\n";
     message += "# HELP sensor_amps Estimated amps from the sensor\n";
     message += "# TYPE sensor_amps gauge\n";
-    message += "sensor_amps{sensor=1} ";
+    message += "sensor_amps{sensor=\"0\"} ";
+    message += last_amps0;
+    message += "\n";
+    message += "sensor_amps{sensor=\"1\"} ";
     message += last_amps1;
-    message += "\n";
-    message += "# HELP sensor_watts Estimated watts from the sensor\n";
-    message += "# TYPE sensor_watts gauge\n";
-    message += "sensor_watts{sensor=2} ";
-    message += last_watts2;
-    message += "\n";
-    message += "# HELP sensor_amps Estimated amps from the sensor\n";
-    message += "# TYPE sensor_amps gauge\n";
-    message += "sensor_amps{sensor=2} ";
-    message += last_amps2;
-    message += "\n";
-    message += "# HELP sensor_watts Estimated watts from the sensor\n";
-    message += "# TYPE sensor_watts gauge\n";
-    message += "sensor_watts{sensor=3} ";
-    message += last_watts3;
-    message += "\n";
-    message += "# HELP sensor_amps Estimated amps from the sensor\n";
-    message += "# TYPE sensor_amps gauge\n";
-    message += "sensor_amps{sensor=3} ";
-    message += last_amps3;
-    message += "\n";
-    message += "# HELP sensor_watts Estimated watts from the sensor\n";
-    message += "# TYPE sensor_watts gauge\n";
-    message += "sensor_watts{sensor=4} ";
-    message += last_watts4;
-    message += "\n";
-    message += "# HELP sensor_amps Estimated amps from the sensor\n";
-    message += "# TYPE sensor_amps gauge\n";
-    message += "sensor_amps{sensor=4} ";
-    message += last_amps4;
     message += "\n";
     return(message);
 }
@@ -133,9 +99,6 @@ void setup()
 
     emon0.current(0, 111.1);             // Current: input pin, calibration.
     emon1.current(1, 111.1);             // Current: input pin, calibration.
-    emon2.current(2, 111.1);             // Current: input pin, calibration.
-    emon3.current(3, 111.1);             // Current: input pin, calibration.
-    emon4.current(4, 111.1);             // Current: input pin, calibration.
 }
 
 void loop()
@@ -176,26 +139,6 @@ void loop()
     last_watts1 = Irms*230.0;
     last_amps1 = Irms;
     
-    Irms = emon2.calcIrms(1480);  // Calculate Irms only
-    Serial.print(Irms*230.0);           // Apparent power
-    Serial.print(" ");
-    Serial.println(Irms);             // Irms
-    last_watts2 = Irms*230.0;
-    last_amps2 = Irms;
-    
-    Irms = emon3.calcIrms(1480);  // Calculate Irms only
-    Serial.print(Irms*230.0);           // Apparent power
-    Serial.print(" ");
-    Serial.println(Irms);             // Irms
-    last_watts3 = Irms*230.0;
-    last_amps3 = Irms;
-    
-    Irms = emon4.calcIrms(1480);  // Calculate Irms only
-    Serial.print(Irms*230.0);           // Apparent power
-    Serial.print(" ");
-    Serial.println(Irms);             // Irms
-    last_watts4 = Irms*230.0;
-    last_amps4 = Irms;
 
     listenForEthernetClients();
 }
