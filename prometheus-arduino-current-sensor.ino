e176a31ca1c7a7b30dcb98de0d70c9e092978099
Trick <trick@vanstaveren.us>
2021-04-24 22:49:17 -0500

    lots of experiments with timing functions to see how consistent they are and suitable for use for capturing mAh

--- prometheus-arduino-current-sensor.ino
+++ prometheus-arduino-current-sensor.ino
@@ -4,19 +4,21 @@
 
 EnergyMonitor emon0;
 EnergyMonitor emon1;
-EnergyMonitor emon2;
-EnergyMonitor emon3;
-EnergyMonitor emon4;
 EthernetServer server(80);
 
 byte mac[] = {
   0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02
 };
 
+unsigned long loop_count = 0;
+unsigned long previous_millis = -1;
+unsigned long last_loop_millis = -1;
 float last_watts0 = -1;
 float last_amps0 = -1;
+unsigned long last_time0 = -1;
 float last_watts1 = -1;
 float last_amps1 = -1;
+unsigned long last_time1 = -1;
 float last_watts2 = -1;
 float last_amps2 = -1;
 float last_watts3 = -1;
@@ -37,6 +39,7 @@ void listenForEthernetClients() {
                 if (c == '\n' && currentLineIsBlank) {
                     client.print(metrics_simple());
                     client.print(metrics_experiment());
+                    client.print(metrics_experiment_2());
                     break;
                 }
                 if (c == '\n') {
@@ -61,15 +64,15 @@ String metrics_simple() {
     String message = "";
     message += "HTTP/1.1 200 OK\n";
     message += "Content-type: text/plain\n\n";
-    message += "# HELP sensor_watts Estimated watts from the sensor\n";
-    message += "# TYPE sensor_watts gauge\n";
+    //message += "# HELP sensor_watts Estimated watts from the sensor\n";
+    /*message += "# TYPE sensor_watts gauge\n";
     message += "sensor_watts{sensor=\"0\"} ";
     message += last_watts0;
     message += "\n";
     message += "sensor_watts{sensor=\"1\"} ";
     message += last_watts1;
-    message += "\n";
-    message += "# HELP sensor_amps Estimated amps from the sensor\n";
+    message += "\n";*/
+    //message += "# HELP sensor_amps Estimated amps from the sensor\n";
     message += "# TYPE sensor_amps gauge\n";
     message += "sensor_amps{sensor=\"0\"} ";
     message += last_amps0;
@@ -80,13 +83,41 @@ String metrics_simple() {
     return(message);
 }
 
+String make_metric_output(String name, String flags, String type, String value) {
+    String message = "";
+    //message += "# HELP " + name + " x\n";
+    message += "# TYPE " + name + " " + type + "\n";
+    message += name;
+    if (flags.length() > 0) {
+        message += "{" + flags + "}";
+    }
+    message += " ";
+    message += value;
+    message += "\n";
+    return(message);
+}
+
 String metrics_experiment() {
     String message = "";
-    message += "# HELP sensor_micros output of micros function\n";
-    message += "# TYPE sensor_micros counter\n";
-    message += "power_sensor_micros ";
-    message += micros();
+    //message += "# HELP sensor_micros output of micros function\n";
+    //m//essage += "# TYPE sensor_micros counter\n";
+   // message += "power_sensor_micros ";
+    //message += micros();
+    //message += "\n";
+
+    message += "# HELP power_sensor_loop_count debug\n";
+    message += "# TYPE power_sensor_loop_count counter\n";
+    message += "power_sensor_loop_count ";
+    message += loop_count;
     message += "\n";
+    return(message);
+}
+
+String metrics_experiment_2() {
+    String message = "";
+    message += make_metric_output("power_sensor_sense_time", "sensor=\"0\"", "gauge", String(last_time0));
+    message += make_metric_output("power_sensor_sense_time", "sensor=\"1\"", "gauge", String(last_time1));
+    message += make_metric_output("power_sensor_loop_time", "", "gauge", String(last_loop_millis));
     return(message);
 }
 
@@ -136,22 +167,32 @@ void loop()
             break;
         }
 
+    unsigned long now = millis();
+    last_loop_millis = now - previous_millis;
+    previous_millis = now;
+    loop_count += 1;
+
     double Irms;
     float voltage = 120.0;
     int sample_factor = 1676;
+
+    unsigned long start_time0 = millis();
     Irms = emon0.calcIrms(sample_factor);  // Calculate Irms only
     Serial.print(Irms*voltage);           // Apparent power
     Serial.print(" ");
     Serial.println(Irms);             // Irms
     last_watts0 = Irms*voltage;
     last_amps0 = Irms;
+    last_time0 = millis() - start_time0;
     
+    unsigned long start_time1 = millis();
     Irms = emon1.calcIrms(sample_factor);  // Calculate Irms only
     Serial.print(Irms*voltage);           // Apparent power
     Serial.print(" ");
     Serial.println(Irms);             // Irms
     last_watts1 = Irms*voltage;
     last_amps1 = Irms;
+    last_time1 = millis() - start_time1;
     
 
     listenForEthernetClients();
